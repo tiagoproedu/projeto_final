ff60d883f839e4c87cee7844a1a79a9b3c7c9b1d
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-07-09 19:56:46 -0300

    add react bootstrap e finalizando estilização da home

---
+++ src/components/Duvidas.jsx
@@ -0,0 +1,96 @@
+import Accordion from 'react-bootstrap/Accordion';
+
+
+function Duvidas() {
+    return (
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Porque eu preciso de uma ferramenta de link na bio?</Accordion.Header>
+                <Accordion.Body>
+                    No momento, toda vez que você tem algo novo para compartilhar,
+                    você precisa ir a cada um dos seus canais para alterar o link
+                    em cada uma das suas biografias. É demorado e complicado tornando
+                    muito mais difícil manter tudo atualizado. Uma ferramenta de link
+                    na biografia significa que você nunca precisa comprometer ou remover
+                    um link da sua biografia para poder adicionar outro. Você pode manter
+                    tudo o que deseja compartilhar online em um link. Quando você tem
+                    uma mudança, você só precisa fazer isso uma vez.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>O linktree é uma ferramenta original de link na bio?</Accordion.Header>
+                <Accordion.Body>
+                    A resposta curta? Sim!
+
+                    Em 2016, criamos o Linktree como uma maneira fácil de vincular a todas
+                    as redes sociais e unificar ecossistemas digitais, sendo pioneiros na
+                    categoria de link na bio. O Linktree continua sendo a principal, maior
+                    e mais popular solução de link na bio mas isso é só o começo. Você pode
+                    usar seu URL ou código QR do Linktree em qualquer lugar que seu público
+                    esteja, incluindo em seus cartões de visita, em sua assinatura de e-mail,
+                    em pôsteres e brochuras em papel e até mesmo em seu currículo. Se você
+                    não tem um site, tudo bem. Se você tem um Linktree, não precisa de um!
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+                <Accordion.Header>O Linktree é seguro para todas as minhas redes sociais?</Accordion.Header>
+                <Accordion.Body>
+                    O Linktree é confiável para todas as plataformas sociais e é usado até
+                    mesmo em muitas das contas de mídia social do Facebook, Instagram e
+                    TikTok! Como o Linktree é a ferramenta de link-in-bio original e mais
+                    popular, a URL linktr.ee é um link confiável, identificável e familiar
+                    no qual o público se sente confortável e seguro para clicar.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+                <Accordion.Header>Como posso direcionar mais tráfego pelo meu Linktree?</Accordion.Header>
+                <Accordion.Body>
+                    Compartilhar seu Linktree em todas as plataformas sociais que você tem
+                    torna mais fácil para seu conteúdo mais importante ser visto e engajado
+                    por todos os seus seguidores. Você pode até usar códigos QR para gerar
+                    tráfego online em lugares offline e direcionar as pessoas para seus links.
+
+                    Assim que os visitantes chegam ao seu Linktree, análises fáceis de entender
+                    ajudam você a descobrir de onde eles vêm e no que estão clicando de forma
+                    rápida e fácil. Você pode ver imediatamente o que está funcionando e o que
+                    não está e melhorar seu Linktree rapidamente com diferentes posicionamentos
+                    de links, links priorizados, subtítulos, animação e muito mais para garantir
+                    que seu tráfego esteja chegando exatamente onde você quer!
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+                <Accordion.Header>Quantos links devo ter no meu Linktree?</Accordion.Header>
+                <Accordion.Body>
+                    Isso depende de duas coisas. Se sua prioridade for cliques e conversão,
+                    recomendamos ter de 3 a 7 links no seu Linktree de uma vez (com base em
+                    nossos criadores mais bem-sucedidos). Incluir muitas opções para seus
+                    visitantes retarda o curso de ação deles.
+
+                    Dito isso: para certos criadores cuja prioridade é exibição, educação
+                    e divulgação (por exemplo, uma gravadora com uma biblioteca de novos
+                    lançamentos para promover ou uma empresa de gestão que busca divulgar
+                    toda a sua lista de clientes), incluir mais de sete links cumpre
+                    perfeitamente seu propósito.
+
+                    Você pode usar recursos do Linktree para adicionar subtítulos, seções,
+                    animações e outros métodos de priorização aos seus links. Assim, não
+                    importa quantas coisas você tenha para compartilhar, você pode direcionar
+                    seus visitantes para o que é mais importante primeiro.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+                <Accordion.Header>Preciso de um site para usar o Linktree?</Accordion.Header>
+                <Accordion.Body>
+                    Não, você não precisa! O Linktree pode atuar como seu próprio mini-site
+                    para compartilhar, vender e crescer sem o tempo e esforço necessários
+                    para construir e manter um site regular. Você pode criar um design que
+                    reflita totalmente sua personalidade e marca em segundos, sem necessidade
+                    de conhecimento, habilidades ou experiência. Se você já tem um site,
+                    isso é ótimo: você pode adicioná-lo ao seu Linktree.
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+    );
+}
+
+export default Duvidas;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,73 +1,68 @@
+import Duvidas from "../../components/Duvidas";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css"
 
 function Home() {
     return (
         <>
-            <div id="mae">
-                <header>
-                    <div className="header">
-                        <div className="logo">
-                            <h1>Linktree</h1>
-                            <img src="src/assets/linktree.svg" alt="icon" />
-                        </div>
-                        <nav>
-                            <ul>
-                                <li>Sobre Nós</li>
-                                <li>Dúvidas</li>
-                                <li>Contatos</li>
-                            </ul>
-                        </nav>
-                        <div id="buttons">
-                            <button className="login">Log in</button>
-                            <button className="signup">Sign Up</button>
-                        </div>
+            <header>
+                <div className="header">
+                    <div className="logo">
+                        <h1>Linktree</h1>
+                        <img src="src/assets/linktree.svg" alt="icon" />
                     </div>
-                </header>
-
-                <main>
-                    <div className="principal">
-                        <div className="conteudoHome">
-                            <div>
-                                <h1>Tudo que você é. Em um simples link na bio.</h1>
-                                <p>Use o linktree no seu instagram, facebook, X, tiktok e muito mais.</p>
-                            </div>
-                            <div className="miniForm">
-                                <input type="text" placeholder="username" name="username" id="username" />
-                                <button>Pegue seu Linktree</button>
-                            </div>
-                        </div>
-                        <div id="imgHome">
-                            <img src="src\assets\home.png" alt="" />
-                        </div>
-                    </div>
-                    <div className="questions">
-                        <h2>Dúvidas Frequentes</h2>
+                    <nav>
                         <ul>
-                            <li>Accordion</li>
-                            <li>Accordion</li>
-                            <li>Accordion</li>
-                            <li>Accordion</li>
-                            <li>Accordion</li>
-                            <li>Accordion</li>
+                            <li>Sobre Nós</li>
+                            <li>Dúvidas</li>
+                            <li>Contatos</li>
                         </ul>
-                    </div>
-                </main>
-
-                <footer>
-                    <div>
+                    </nav>
+                    <div className="buttons">
                         <button className="login">Log in</button>
                         <button className="signup">Sign Up</button>
                     </div>
-                    <div className="socialMedias">
-                        <ul>
-                            <li>Instagram</li>
-                            <li>Facebook</li>
-                            <li>X</li>
-                            <li>TikTok</li>
-                        </ul>
+                </div>
+            </header>
+
+            <main>
+                <div className="principal">
+                    <div className="conteudoHome">
+                        <div>
+                            <h1>Tudo que você é. Em um simples link na bio.</h1>
+                            <p>Use o linktree no seu instagram, facebook, X, tiktok e muito mais.</p>
+                        </div>
+                        <div className="miniForm">
+                            <input type="text" placeholder="username" name="username" id="username" />
+                            <button>Pegue seu Linktree</button>
+                        </div>
                     </div>
-                </footer>
-            </div>
+                    <div id="imgHome">
+                        <img src="src\assets\home.png" alt="" />
+                    </div>
+                </div>
+                <div className="questions">
+                    <div className="conteudoQuestions">
+                        <h2>Dúvidas Frequentes</h2>
+                        <Duvidas />
+                    </div>
+                </div>
+            </main>
+
+            <footer>
+                <div className="buttons">
+                    <button className="login">Log in</button>
+                    <button className="signup">Sign Up</button>
+                </div>
+                <div className="socialMedias">
+                    <ul>
+                        <li>Instagram</li>
+                        <li>Facebook</li>
+                        <li>X</li>
+                        <li>TikTok</li>
+                    </ul>
+                </div>
+            </footer>
         </>
     );
 }
