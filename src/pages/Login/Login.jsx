ef6ee69731d900f23396cff8b5e116d94fd6246c
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-07-12 19:27:11 -0300

    finalizando page login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,40 +4,35 @@ function Login() {
     return (
         <>
             <div className="telaLogin">
-                <main>
-                    <div className="metadeForm">
-                        <nav id="logo">
-                            <Logo />
-                        </nav>
-                        <div className="infos">
-                            <div className="mensagem">
-                                <h1>Bem-vindo!</h1>
-                                <p>Faça login no seu linktree!</p>
+                <div className="metadeForm">
+                    <nav id="logo">
+                        <Logo />
+                    </nav>
+                    <div className="infos">
+                        <div className="mensagem">
+                            <h1>Bem-vindo!</h1>
+                            <p>Faça login no seu linktree!</p>
+                        </div>
+                        <div id="infoLogin">
+                            <form action="">
+                                <input type="text" placeholder="Username" name="username" id="username" />
+                                <input type="password" placeholder="Senha" name="password" id="password" />
+                            </form>
+                            <a href="">Log in com o número de celular</a>
+                            <button>Log in</button>
+                            <div className="esqueceu">
+                                <a href="" className="senha">Esqueceu a senha?</a>
+                                <a href="" className="usuario">Esqueceu o usuario?</a>
                             </div>
-                            <div id="infoLogin">
-                                <form action="">
-                                    <input type="text" placeholder="Username" name="username" id="username" />
-                                    <input type="password" placeholder="Senha" name="password" id="password" />
-                                </form>
-                                <a href="">Log in com o número de celular</a>
-                                <button id="login">Log in</button>
-                                <div className="esqueceu">
-                                    <a href="" className="senha">Esqueceu a senha?</a>
-                                    <a href="" className="usuario">Esqueceu o usuario?</a>
-                                </div>
-                                <div className="semConta">
-                                    <a href="">Ainda não tem uma conta?</a>
-                                </div>
+                            <div className="semConta">
+                                <a href="">Ainda não tem uma conta?</a>
                             </div>
                         </div>
                     </div>
-                </main>
-
-                <aside>
-                    <div>
-                        <img src="src\assets\login.png" alt="login" />
-                    </div>
-                </aside>
+                </div>
+                <div id="imgLogin">
+                    <img src="src\assets\login.png" alt="login" />
+                </div>
             </div>
         </>
     );
