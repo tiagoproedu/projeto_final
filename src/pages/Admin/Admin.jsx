f2373346b8f52339a1b3acf4474d09eb2df40d74
Tiago da Silva Barbosa <tiagoproedu@gmail>
2024-07-15 12:03:28 -0300

    estilizando admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,40 +3,46 @@ import "./style.css"
 function Admin() {
     return (
         <>
-            <div id="aside">
-                <div><button>Links</button></div>
-                <div><button>Aparência</button></div>
-                <div><button>Analíticos</button></div>
-                <div><button>Configurações</button></div>
-                <div><button>Usuário</button></div>
-            </div>
+            <div id="admin">
+                <div id="aside">
+                    <div className="linkButtons">
+                        <button>Links</button>
+                        <button>Aparência</button>
+                        <button>Analíticos</button>
+                        <button>Configurações</button>
+                    </div>
+                    <div className="userButton">
+                        <div><button>Usuário</button></div>
+                    </div>
+                </div>
 
-            <div id="main">
-                <div>
-                    <button>Adicionar link</button>
-                    <div className="cardLink">
-                        <div>
-                            <h1>titulo</h1>
-                            <a href="">link</a>
+                <div id="main">
+                    <div>
+                        <button>Adicionar link</button>
+                        <div className="cardLink">
                             <div>
-                                <p>compartilhar</p>
-                                <p>button on/off</p>
+                                <h1>titulo</h1>
+                                <a href="">link</a>
+                                <div>
+                                    <p>compartilhar</p>
+                                    <p>button on/off</p>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div className="icones">
+                            <div>
+                                <div className="icones">
 
-                            </div>
-                            <div className="lixeira">
+                                </div>
+                                <div className="lixeira">
 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div id="viewer">
+                <div id="viewer">
 
+                </div>
             </div>
         </>
     );
