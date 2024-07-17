ec3d7c35bb6d1d42f06ba7fe2462ec7c149916a0
João Levi <jlevi.gt@gmail.com>
2024-07-17 13:24:15 +0000

    add fetch api cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,32 +1,70 @@
+import { useRef } from "react";
+
 import Logo from "../../components/Logo/Logo";
+
+import "./style.css";
+
+const tcpip = "localhost:8080";
+
 function Cadastro() {
-    return (
-        <>
-            <div className="telaLogin">
-                <div className="metadeForm">
-                    <nav id="logo">
-                        <Logo />
-                    </nav>
-                    <div className="infos">
-                        <div className="mensagem">
-                            <h1>Se junte ao Linktree</h1>
-                            <p>Faça seu cadastro no linktree!</p>
-                        </div>
-                        <div id="infoLogin">
-                            <form method="post" action="/admin">
-                                <input type="text" placeholder="Username" name="email" id="username" />
-                                <input type="password" placeholder="Senha" name="password" id="password" />
-                                <button type="submit">Cadastrar-se</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div id="imgLogin">
-                    <img src="src\assets\cadastro.png" alt="login" />
-                </div>
-            </div>
-        </>
-    );
+  const usernameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const username = usernameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    try {
+      const response = await fetch(`${tcpip}/api/v1/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+    } catch (error) {
+      alert("Alguma coisa deu errado");
+    }
+  }
+
+  return (
+    <div className="screen">
+      <div className="half-screen--form">
+        <nav className="logo">
+          <Logo />
+        </nav>
+
+        <div className="info-section">
+          <div className="message">
+            <h1>Se junte ao Linktree</h1>
+            <p>Faça seu cadastro no linktree!</p>
+          </div>
+
+          <div className="signup-form">
+            <form method="post" onSubmit={handleSubmit}>
+              <input type="text" placeholder="Username" name="username" id="username" ref={usernameRef} />
+              <input type="text" placeholder="email" name="email" id="email" ref={emailRef} />
+              <input type="password" placeholder="Senha" name="password" id="password" ref={passwordRef} />
+
+              <button type="submit">Cadastrar-se</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="half-screen--image">
+        <img src="src/assets/cadastro.png" alt="login" />
+      </div>
+    </div>
+  );
 }
 
 export default Cadastro;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,74 +1,93 @@
-import Duvidas from "../../components/Duvidas";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./style.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
 import Logo from "../../components/Logo/Logo";
+import Duvidas from "../../components/Duvidas";
 
 function Home() {
-    return (
-        <>
-            <header>
-                <div className="header">
-                    <Logo />
-                    <nav>
-                        <ul>
-                            <li><a href="#sobrenos">Sobre Nós</a></li>
-                            <li><a href="#duvidas">Dúvidas</a></li>
-                            <li><a href="#contatos">Contatos</a></li>
-                        </ul>
-                    </nav>
-                    <div className="buttons">
-                        <button className="login"><a href="login">Log in</a></button>
-                        <button className="signup"><a href="cadastro">Sign Up</a></button>
-                    </div>
-                </div>
-            </header>
+  return (
+    <>
+      <header>
+        <div className="header">
+          <Logo />
+          <nav>
+            <ul>
+              <li>
+                <a href="#sobrenos">Sobre Nós</a>
+              </li>
+              <li>
+                <a href="#duvidas">Dúvidas</a>
+              </li>
+              <li>
+                <a href="#contatos">Contatos</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="buttons">
+            <button className="login">
+              <a href="login">Log in</a>
+            </button>
+            <button className="signup">
+              <a href="cadastro">Sign Up</a>
+            </button>
+          </div>
+        </div>
+      </header>
 
-            <main>
-                <section id="sobrenos">
-                    <div className="principal">
-                        <div className="conteudoHome">
-                            <div>
-                                <h1>Tudo que você é. Em um simples link na bio.</h1>
-                                <p>Use o linktree no seu instagram, facebook, X, tiktok e muito mais.</p>
-                            </div>
-                            <div className="miniForm">
-                                <input type="text" placeholder="username" name="username" id="username" />
-                                <button>Pegue seu Linktree</button>
-                            </div>
-                        </div>
-                        <div id="imgHome">
-                            <img src="src\assets\home.png" alt="imgHome" />
-                        </div>
-                    </div>
-                </section>
-                <section id="duvidas">
-                    <div className="questions">
-                        <div className="conteudoQuestions">
-                            <h2>Dúvidas Frequentes</h2>
-                            <Duvidas />
-                        </div>
-                    </div>
-                </section>
-            </main>
+      <main>
+        <section id="sobrenos">
+          <div className="principal">
+            <div className="conteudoHome">
+              <div>
+                <h1>Tudo que você é. Em um simples link na bio.</h1>
+                <p>Use o linktree no seu instagram, facebook, X, tiktok e muito mais.</p>
+              </div>
+              <div className="miniForm">
+                <input type="text" placeholder="username" name="username" id="username" />
+                <button>Pegue seu Linktree</button>
+              </div>
+            </div>
+            <div id="imgHome">
+              <img src="src\assets\home.png" alt="imgHome" />
+            </div>
+          </div>
+        </section>
+        <section id="duvidas">
+          <div className="questions">
+            <div className="conteudoQuestions">
+              <h2>Dúvidas Frequentes</h2>
+              <Duvidas />
+            </div>
+          </div>
+        </section>
+      </main>
 
-            <footer>
-                <div className="buttons">
-                    <button className="login">Log in</button>
-                    <button className="signup">Sign Up</button>
-                </div>
-                <section id="contatos">
-                    <div className="socialMedias">
-                        <ul>
-                            <li><a href="https://www.instagram.com/linktr.ee/">Instagram</a></li>
-                            <li><a href="https://www.facebook.com/LinktreeBrasil">Facebook</a></li>
-                            <li><a href="https://x.com/linktreebrasil">X</a></li>
-                            <li><a href="https://tiktok.com/@linktr.ee">Tiktok</a></li>
-                        </ul>
-                    </div>
-                </section>
-            </footer>
-        </>
-    );
+      <footer>
+        <div className="buttons">
+          <button className="login">Log in</button>
+          <button className="signup">Sign Up</button>
+        </div>
+        <section id="contatos">
+          <div className="socialMedias">
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/linktr.ee/">Instagram</a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/LinktreeBrasil">Facebook</a>
+              </li>
+              <li>
+                <a href="https://x.com/linktreebrasil">X</a>
+              </li>
+              <li>
+                <a href="https://tiktok.com/@linktr.ee">Tiktok</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </footer>
+    </>
+  );
 }
 
 export default Home;
