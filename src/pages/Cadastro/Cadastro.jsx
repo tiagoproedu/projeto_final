47e7b07baa3381f5cfbfb882747c175544af951b
jlevigt <jlevi.gt@gmail.com>
2024-07-22 17:36:42 -0300

    add refs

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,5 +1,42 @@
+import { useRef } from "react";
+
 import Logo from "../../components/Logo/Logo";
+
+const tcpip = "http://localhost:8080";
+
 function Cadastro() {
+  const nameRef = useRef("");
+  const usernameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const name = nameRef.current.value;
+    const username = usernameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    try {
+      const response = await fetch(`${tcpip}/api/v1/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert("Alguma coisa deu errado");
+    }
+  }
   return (
     <>
       <div className="telaLogin">
@@ -13,25 +50,34 @@ function Cadastro() {
               <p>Faça seu cadastro no linktree!</p>
             </div>
             <div id="infoLogin">
-              <form method="post" action="/admin">
-                <input type="text" placeholder="Name" name="name" id="name" />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  id="name"
+                  ref={nameRef}
+                />
                 <input
                   type="text"
                   placeholder="Username"
                   name="username"
                   id="username"
+                  ref={usernameRef}
                 />
                 <input
                   type="email"
                   placeholder="Email"
                   name="email"
                   id="email"
+                  ref={emailRef}
                 />
                 <input
                   type="password"
                   placeholder="Senha"
                   name="password"
                   id="password"
+                  ref={passwordRef}
                 />
                 <button type="submit">Cadastrar-se</button>
               </form>
